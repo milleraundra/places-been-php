d0478933867434530b178fae7e0095118f7fa232
aundra miller and jared beckler <miller.aundra@gmail.com>
2016-02-10 11:33:28 -0800

    add finished web app

---
+++ src/Place.php
@@ -0,0 +1,39 @@
+<?php
+class Place
+{
+    private $city;
+
+    function __construct($city_name)
+    {
+        $this->city = $city_name;
+    }
+
+    function setCity($city_name)
+    {
+        $this->city = $city_name;
+    }
+
+    function getCity()
+    {
+        return $this->city;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+    static function deleteAll()
+    {
+        return $_SESSION['list_of_places'] = array();
+    }
+
+}
+
+
+ ?>
